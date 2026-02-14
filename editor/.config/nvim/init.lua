32a715e6a0118d15aa9bd28017efd21a369cf1a1
lightclient <lightclient@protonmail.com>
2026-02-13 19:28:41 -0700

    lua_ls: add vim to diagnostics globals to suppress undefined warnings

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -862,8 +862,9 @@ require("lazy").setup({
 							completion = {
 								callSnippet = "Replace",
 							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
+							diagnostics = {
+								globals = { "vim" },
+							},
 						},
 					},
 				},
