12602967675b4c527019efebcfc03cf08b59b190
lightclient <lightclient@protonmail.com>
2026-02-13 19:32:39 -0700

    replace Comment.nvim with built-in gc/gcc, clean up avante dependencies
    - Remove Comment.nvim plugin (nvim 0.10+ has native commenting)
    - Remap <leader>/ to built-in gcc/gc
    - Remove unused avante deps (copilot, fzf-lua, mini.pick, img-clip, web-devicons)
    - Remove commented-out provider configs

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -174,6 +174,10 @@ vim.opt.linebreak = true
 -- Press jk fast to ESC
 vim.keymap.set("i", "jk", "<ESC>")
 
+-- Toggle comments (built-in gc/gcc)
+vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment line" })
+vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Toggle comment" })
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
@@ -1173,31 +1177,13 @@ require("lazy").setup({
 		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
 		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
+
 	{
-		"numToStr/Comment.nvim",
-		config = function()
-			vim.keymap.set(
-				"n",
-				"<leader>/",
-				"<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
-				{ desc = "Toggle comment line" }
-			)
-			vim.keymap.set(
-				"v",
-				"<leader>/",
-				"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-				{ desc = "Toggle comment line" }
-			)
-		end,
-	},
-	{
-		-- Adds Cursor AI like expereince in neovim.
 		"yetone/avante.nvim",
 		event = "VeryLazy",
-		version = false, -- Never set this value to "*"! Never!
+		version = false,
+		build = "make",
 		opts = {
-			-- add any opts here
-			-- for example
 			provider = "openrouter",
 			vendors = {
 				openrouter = {
@@ -1207,61 +1193,15 @@ require("lazy").setup({
 					model = "google/gemini-2.5-pro-preview-03-25",
 				},
 			},
-			-- claude = {
-			-- 	endpoint = "https://api.anthropic.com",
-			-- 	model = "claude-3-5-sonnet-20241022",
-			-- 	timeout = 30000, -- Timeout in milliseconds
-			-- 	temperature = 0,
-			-- 	max_tokens = 4096,
-			-- 	disable_tools = true, -- disable tools!
-			-- },
-			-- openai = {
-			-- 	endpoint = "https://api.openai.com/v1",
-			-- 	model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-			-- 	timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-			-- 	temperature = 0,
-			-- 	max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-			-- 	--reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-			-- },
 		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		build = "make",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"stevearc/dressing.nvim",
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
-			"echasnovski/mini.pick", -- for file_selector provider mini.pick
-			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-			"ibhagwan/fzf-lua", -- for file_selector provider fzf
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"zbirenbaum/copilot.lua", -- for providers='copilot'
 			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
-			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
 				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
+				opts = { file_types = { "markdown", "Avante" } },
 				ft = { "markdown", "Avante" },
 			},
 		},
